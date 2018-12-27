be5529eec8a7e0287472ff79e9703641f56d1f8b
k8hertweck <k8hertweck@gmail.com>
2018-12-26 22:48:10 -0800

    content for analyzing patient data

--- SWC/python.py
+++ SWC/python.py
@@ -35,32 +35,127 @@
 
 # Objectives: intro to libraries, read in data, assign values to variables, select data values, operations on arrays, simple graphs
 
+# include human-readable (but not python interpreted) comments following hash signs
+
 # use python as calculator
 3 + 5
-
-# libraries
-import numpy
 # shift+enter to execute command
 
+# assign value to variable
+weight_kg = 60
+# variable names:
+#   can include letters, digits, and underscores
+#   cannot start with a digit
+#   are case sensitive
+
+# data types:
+#   integers
+#   floating point numbers (decimals)
+#   strings (characters)
+# weight_kg is integer
+# to create as floating point
+weight_kg = 60.0
+# to create string
+weight_kg_text = 'weight in kilograms:'
+
+# display value of variable
+print(weight_kg)
+# print is a function
+# can display multiple items at once
+print(weight_kg_text, weight_kg)
+
+# perform arithmetic inside print function
+print('weight in pounds:', 2.2 * weight_kg)
+# note: this doesn't change the value of weight_kg!
+print(weight_kg)
+
+# assign new value to weight_kg
+weight_kg = 65.0
+print('weight in kilograms is now:', weight_kg)
+# variable names as sticky notes (analogy)
+
+# libraries: describe what they are
+# load library
+import numpy
+
+# load data into python (using library)
 numpy.loadtxt(fname='inflammation-01.csv', delimiter=',')
-first part is function call: function from particular library
-dotted notation: numpy.loadtxt(...)
-parameters: name of file, delimiter
-assign variable:
-weight_kg = 55
-print weight_kg
-case sensitive
-arithmetic:
-print 'weight in pounds:', 2.2 * weight_kg
-separated with commas
-change variable by assigning new
-weight_kg =57.4
-print 'weight in kilograms is now:', weight_kg
-garbage
+# numpy.loadtxt(...) is a function call
+#   run function loadtxt
+#   belongs to numpy library
+#   dotted notation in function call means whatever appears before dot contains the thing after the dot
+# parameters: specific information that is sent to (passed) to function call
+#   name of file
+#   delimiter
 
-####FILES AND DIRECTORIES
-Objectives: paths, learn basic commands for working with files and directories, learn syntax of commands, tab-completion
+# assign data to variable (so we can recall it later)
+data = numpy.loadtxt(fname='inflammation-01.csv', delimiter=',')
+# show the variable's value
+print(data)
+# what type of thing is data?
+print(type(data))
+# find type of data contained within array (data)
+print(data.dtype)
+# show shape of data
+print(data.shape)
+# output is rows, columns
+# arrays have members, or attributes, which use the dot nomenclature because they have the same part-and-whole relationship
 
+# access a specific value
+print('first value in data:', data[0, 0])
+# python begins indexing (counting) at 0
+print('middle value in data:', data[30, 20])
 
-####END CLASS
-sticky note feedback
+# select sections of data (slicing)
+print(data[0:4, 0:10])
+# end bound is NOT inclusive (up to but not including)
+# can start at indeces besides 0
+print(data[5:10, 0:10])
+# use empty bound to include the end of axis
+small = data[:3, 36:] # assign to value
+print('small is:')
+print(small)
+
+# perform math on array
+doubledata = data * 2.0
+# view output
+print('original:')
+print(data[:3, 36:])
+print('doubledata:')
+print(doubledata[:3, 36:])
+
+# perform operation involving two arrays
+tripledata = doubledata + data
+print('tripledata:')
+print(tripledata[:3, 36:])
+
+# perform calculation across entire array
+print(numpy.mean(data)) # find mean
+
+# use multiple assignment to obtain descriptive values of data
+maxval, minval, stdval = numpy.max(data), numpy.min(data), numpy.std(data)
+
+print('maximum inflammation:', maxval)
+print('minimum inflammation:', minval)
+print('standard deviation:', stdval)
+
+# to find available functions and information:
+#   type name of something, followed by dot, then hit tab (ipython and notebooks)
+#   select a function or attribute and add question mark to find help documentation
+#   help(thing.attribute) is same as above
+
+# create temporary array for data desired
+patient_0 = data[0, :] # 0 on the first axis (rows), everything on the second (columns)
+print('maximum inflammation for patient 0:', numpy.max(patient_0))
+
+# combine selection and function call (skip temp variable)
+print('maximum inflammation for patient 2:', numpy.max(data[2, :]))
+
+# average across all rows (axis 0)
+print(numpy.mean(data, axis=0))
+# confirm shape of array
+print(numpy.mean(data, axis=0).shape)
+# average across all columns (axis 1)
+print(numpy.mean(data, axis=1))
+
+#### Visualizing data ####
