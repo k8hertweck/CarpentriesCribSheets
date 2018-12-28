86501e1f754acb70436f6efaad44ae41e881f21b
k8hertweck <k8hertweck@gmail.com>
2018-12-27 16:01:41 -0800

    adding analyzing multiple files

--- SWC/python.py
+++ SWC/python.py
@@ -350,3 +350,41 @@ sond = months[8:len(months)]
 print("Using len() to get last entry:", sond)
 sond = months[8:]
 print("Omitting ending index:", sond)
+
+#### Analyzing data from multiple files ####
+
+# import library to find files/directories
+import glob
+
+# get names of all csv files in current directory
+print(glob.glob('inflammation*.csv'))
+
+# combine previous content together and analyze all files
+
+import numpy
+import matplotlib.pyplot
+
+filenames = sorted(glob.glob('inflammation*.csv'))
+filenames = filenames[0:3]
+for f in filenames:
+    print(f)
+
+    data = numpy.loadtxt(fname=f, delimiter=',')
+
+    fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
+
+    axes1 = fig.add_subplot(1, 3, 1)
+    axes2 = fig.add_subplot(1, 3, 2)
+    axes3 = fig.add_subplot(1, 3, 3)
+
+    axes1.set_ylabel('average')
+    axes1.plot(numpy.mean(data, axis=0))
+
+    axes2.set_ylabel('max')
+    axes2.plot(numpy.max(data, axis=0))
+
+    axes3.set_ylabel('min')
+    axes3.plot(numpy.min(data, axis=0))
+
+    fig.tight_layout()
+    matplotlib.pyplot.show()
